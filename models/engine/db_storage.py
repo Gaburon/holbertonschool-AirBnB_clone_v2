208c57aa61b52d7c4149e2aac4f18c17db5d99a2
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-17 20:36:06 +0000

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,59 +16,58 @@ from models.place import Place
 
 class DBStorage:
 
-  __engine = None
-  __session = None
+    __engine = None
+    __session = None
 
-  def __init__(self):
-    user = os.environ.get('HBNB_MYSQL_USER')
-    pwd = os.environ.get('HBNB_MYSQL_PWD')
-    host = os.environ.get('HBNB_MYSQL_HOST')
-    db = os.environ.get('HBNB_MYSQL_DB')
-    self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
-                                user, pwd, host, db), pool_pre_ping=True)
-    if os.environ.get('HBNB_ENV') == 'test':
-      Base.metadata.drop_all(bind=self.__engine)
+    def __init__(self):
+        user = os.environ.get('HBNB_MYSQL_USER')
+        pwd = os.environ.get('HBNB_MYSQL_PWD')
+        host = os.environ.get('HBNB_MYSQL_HOST')
+        db = os.environ.get('HBNB_MYSQL_DB')
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+                                    user, pwd, host, db), pool_pre_ping=True)
+        if os.environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(bind=self.__engine)
 
-  def all(self, cls=None):
-      """
-      Perform query on the current database session
-      # Must return a dictionary with all objects according
-      to class name passed in cls argument
-      """
-      if cls is not None:
-          objs = self.__session.query(cls).all()
-      else:
-          objs = self.__session.query(Amenity).all()
-          objs += self.__session.query(City).all()
-          objs += self.__session.query(Place).all()
-          objs += self.__session.query(Review).all()
-          objs += self.__session.query(State).all()
-          objs += self.__session.query(User).all()
-      return {"{}.{}".format(obj.__class__.__name__, obj.id): obj
-              for obj in objs}
+    def all(self, cls=None):
+        """
+        Perform query on the current database session
+        # Must return a dictionary with all objects according
+        to class name passed in cls argument
+        """
+        if cls is not None:
+            objs = self.__session.query(cls).all()
+        else:
+            objs = self.__session.query(Amenity).all()
+            objs += self.__session.query(City).all()
+            objs += self.__session.query(Place).all()
+            objs += self.__session.query(Review).all()
+            objs += self.__session.query(State).all()
+            objs += self.__session.query(User).all()
+        return {"{}.{}".format(obj.__class__.__name__, obj.id): obj
+                for obj in objs}
 
-  def new(self, obj):
-    self.__session.add(obj)
+    def new(self, obj):
+        self.__session.add(obj)
 
-  def save(self):
-    self.__session.commit()
+    def save(self):
+        self.__session.commit()
 
-  def delete(self, obj=None):
-    if obj != None:
-      self.__session.delete(obj)
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
 
-  def reload(self):
-      """
-      Commit all changes in database after
-      the changings
-      """
-      Base.metadata.create_all(self.__engine)
-      session_factory = sessionmaker(
-          bind=self.__engine, expire_on_commit=False)
-      Session = scoped_session(session_factory)
-      self.__session = Session
+    def reload(self):
+        """
+        Commit all changes in database after
+        the changings
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session
 
-  def close(self):
-      """close session, proper ending"""
-      self.__session.remove()
-  
+    def close(self):
+        """close session, proper ending"""
+        self.__session.remove()
