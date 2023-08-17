55b4fd0256a6aa5227a68efc3bc24ca4e2e7b2f1
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-17 13:45:43 -0500

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -10,18 +10,22 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    '''First function. Prints on /'''
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    '''Second function. Prints on /hbnb'''
     return "HBNB"
 
 @app.route("c/<text>", strict_slashes=False)
 def c(text):
+    '''Third function. Prints on /c/anything'''
     return f"C {text.replace('_', ' ')}"
 
 @app.route("/python/<text>", strict_slashes=False)
 def pyton(text):
+    '''Fourth function. Prints on /python/anything'''
     return f"Python { text.replace('_', ' ')}"
 
 if __name__ == '__main__':
