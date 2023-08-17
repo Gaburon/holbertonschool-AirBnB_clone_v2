76fa992d9fcb9c2a1ed5f32f9123eabca529c2b1
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-17 13:51:13 -0500

    Create 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''Flask WSGI for multiple routes'''
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''First function. Prints on /'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    '''Second function. Prints on /hbnb'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''Third function. Prints on /c/anything'''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text='is cool'):
+    '''Fourth function. Prints on /python/anything'''
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def only_int(n):
+    '''Fifth function. Prints on number if n is int'''
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
