3b5954299f5521284238122b2c2431fb767e7185
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-18 17:34:05 -0500

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,6 +20,5 @@ def cities():
     states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states )
 
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
